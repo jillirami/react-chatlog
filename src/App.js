111db6754e679ac7b242eada066ac7af3c20c195
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-06-11 16:35:58 -0700

    removed component import from react on app component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 import MessageHistory from './components/MessageHistory';
